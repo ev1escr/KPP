ed874f837ad3cfef7d1e7019a44149f0e61a8849
Hanna Akhrameika <Hanna.Akhrameika@thomsonreuters.com>
2022-03-02 19:26:24 +0300

    final commit

--- src/main/java/com/eviescr/controller/CalculateController.java
+++ src/main/java/com/eviescr/controller/CalculateController.java
@@ -1,7 +1,10 @@
 package com.eviescr.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculateController {
