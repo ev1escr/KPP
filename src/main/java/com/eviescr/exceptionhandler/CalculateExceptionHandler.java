6796627557eb3ca3ebbce31ed481a8c643787640
Evie <logovokolyana@gmail.com>
2022-03-27 23:06:13 +0300

    lab2

--- src/main/java/com/eviescr/exceptionhandler/CalculateExceptionHandler.java
+++ src/main/java/com/eviescr/exceptionhandler/CalculateExceptionHandler.java
@@ -1,4 +1,34 @@
 package com.eviescr.exceptionhandler;
 
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.apache.logging.log4j.LogManager;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
 public class CalculateExceptionHandler {
+
+    private static final Logger logger = LogManager.getLogger(CalculateExceptionHandler.class);
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<?> handlerBadRequestException(MissingServletRequestParameterException e) {
+        ExceptionInfo error = new ExceptionInfo(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+
+        logger.info("MissingServletRequestParameterException");
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> handlerBadRequestException(MethodArgumentTypeMismatchException e) {
+        ExceptionInfo error = new ExceptionInfo(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+
+        logger.info("MethodArgumentTypeMismatchException");
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
 }
--- src/test/java/com/eviescr/CalculateUnitTest.java
+++ src/test/java/com/eviescr/CalculateUnitTest.java
@@ -1,27 +1,127 @@
 package com.eviescr;
 
-import com.eviescr.dto.CalculateResultDto;
-import com.eviescr.service.CalculateService;
+//import com.eviescr.controller.CalculatorController;
+//import com.eviescr.dto.CalculateResultDto;
+//import com.eviescr.service.CalculateService;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class CalculateUnitTest {
+//
+//    @InjectMocks
+//    private CalculatorController controller;
+//
+//    @Mock
+//    private CalculateService service;
+//
+//    @Test
+//    public void Given_Num1AndNum2_When_CalculateSumAndMultiply_Then_ResultIsReturned() {
+//        Double num1 = 4D;
+//        Double num2 = 3D;
+//        CalculateResultDto calculateDto = new CalculateResultDto(num1, num2);
+//        double actualSum = calculateDto.getSum();
+//        double actualMultiply = calculateDto.getMultiply();
+//
+//        assertEquals(service.getCalculation(num1, num2).getMultiply(), actualMultiply);
+//        assertEquals(service.getCalculation(num1, num2).getSum(), actualSum);
+//    }
+//
+//    @Test
+//    public void shouldReturnBadRequest() {
+//        double num1 = 4D;
+//        String num2 = "zxc23";
+//
+//
+//    }
+//
+//}
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@ExtendWith(MockitoExtension.class)
+import javax.servlet.ServletContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class CalculateUnitTest {
 
-    @InjectMocks
-    private CalculateService service;
+    private final String ENDPOINT = "/calculate";
+
+
+    @Autowired
+    private WebApplicationContext applicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.applicationContext).build();
+    }
 
     @Test
-    public void Given_Num1AndNum2_When_CalculateSumAndMultiply_Then_ResultIsReturned() {
-        Double num1 = 4D;
-        Double num2 = 3D;
-        CalculateResultDto calculateDto = new CalculateResultDto(num1, num2);
+    public void givenServletContext_whenInitialize_thenContextExists() {
+        ServletContext servletContext = applicationContext.getServletContext();
 
-        assertEquals(service.getCalculation(num1, num2).getMultiply(), calculateDto.getMultiply());
-        assertEquals(service.getCalculation(num1, num2).getSum(), calculateDto.getSum());
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        assertNotNull(applicationContext.getBean("calculatorController"));
+    }
+
+    @Test
+    public void shouldReturnCalculateResultDto() throws Exception {
+        String params = "?num1=4&num2=5";
+        this.mockMvc.perform(get(ENDPOINT + params))
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.sum").value(9))
+                .andExpect(jsonPath("$.multiply").value(20))
+                .andExpect(jsonPath("$.num1").value(4))
+                .andExpect(jsonPath("$.num2").value(5));
+    }
+
+    @Test
+    public void shouldThrowsMethodArgumentTypeMismatchException() throws Exception {
+        String params = "?num1=4&num2=zxc5";
+        this.mockMvc.perform(get(ENDPOINT + params))
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Failed to convert value of type " +
+                        "'java.lang.String' to required type 'java.lang.Double'; nested exception is " +
+                        "java.lang.NumberFormatException: For input string: \"zxc5\""));
+    }
+
+    @Test
+    public void shouldThrowsMissingServletRequestParameterException() throws Exception {
+        String params = "?num2=5";
+        this.mockMvc.perform(get(ENDPOINT + params))
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Required request parameter 'num1' " +
+                        "for method parameter type Double is not present"));
     }
 
 }
+
